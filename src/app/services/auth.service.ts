e779eeea1b0f165a4609ed16672f4c2041966a20
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-16 15:10:53 -0500

    kk2: Add types to collection and fix issues

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private usersCollection: AngularFirestoreCollection = this.db.collection<IUser>('users');
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) { }
 
   public async createUser(userData: IUser) {
+    if(!userData.password) {
+      throw new Error('Password is required !')
+    }
     const userCredentials = await this.auth.createUserWithEmailAndPassword(
       userData.email as string, userData.password as string
     );
-    this.db.collection('users').add({
+    await this.usersCollection.add({
       name: userData.name,
       email: userData.email,
       age: userData.age,
