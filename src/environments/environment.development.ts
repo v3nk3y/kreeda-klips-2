05c089c19445bf716a851cb17e6de78ea936d30d
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-16 14:09:28 -0500

    kk2: Install and add firebase config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA0zBedE4IGwi-8-AXrPXFtQCb8hng4XkU",
+        authDomain: "kreeda-klips-2.firebaseapp.com",
+        projectId: "kreeda-klips-2",
+        storageBucket: "kreeda-klips-2.appspot.com",
+        // messagingSenderId: "1046250253484",
+        appId: "1:1046250253484:web:3e765fe7b2a2a548f58309",
+        measurementId: "G-H7SNCZH6S4"
+    }
+};
