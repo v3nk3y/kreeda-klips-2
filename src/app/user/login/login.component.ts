d4f54afecca84f71ac6fe89ff62f0b771bf75574
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-20 14:40:24 -0500

    kk2: signinwithEmailAdnPAssword addition

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,34 @@ export class LoginComponent {
     email: '',
     password: '',
   }
-
   emailPattern = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}';
+  showAlert = false;
+  alertMsg = 'Please wait! We are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
 
-  login() {
-    console.log(this.credentials);
+  constructor(private auth: AngularFireAuth) {}
+
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! We are logging you in.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+    
+    try {
+      await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
+    } catch (error) {
+      this.alertMsg = 'An unexpected error occur. Please try again later!'
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      this.showAlert = true;
+      console.log(error);
+      return;
+    }
+    
+    this.alertMsg = 'Success! You are logged in.'
+    this.alertColor = 'green';
+    this.inSubmission = false;
+    this.showAlert = true;
   }
 }
