16ba78f2f2d4c95711347692855e8a7e093cd8d7
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-20 14:54:26 -0500

    kk2: checkfor logged in user and close modal

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { delay, map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,16 @@ import { map } from 'rxjs/operators'
 export class AuthService {
   private usersCollection: AngularFirestoreCollection = this.db.collection<IUser>('users');
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) { 
     auth.user.subscribe(val => console.log(val));
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
     )
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
+      delay(1000)
+    );
   }
 
   public async createUser(userData: IUser) {
