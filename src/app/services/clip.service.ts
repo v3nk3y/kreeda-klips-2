854db7d9cc96af58af779aca99f43adb9a281653
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-21 15:26:19 -0500

    kk2: Query db to get the user clips collection for manage component

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +11,28 @@ import IClip from '../models/clip.model';
 export class ClipService {
   public clipsCollection!: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
     this.clipsCollection = this.db.collection('clips');
    }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return  this.clipsCollection.add(data);
-   }
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap(user => {
+        // If user is null then return an empty array obj as SMap expects an Observable object to subscribe
+        if(!user){
+          return of([]);
+        }
+
+        // To generate a query form the the collection to grap user's clips based on the uid of user
+        const query = this.clipsCollection.ref.where('uid', "==", user.uid);
+
+        // Run the query and retunr the promise object
+        return query.get();
+      })
+    );
+  }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-// import { ClipService } from 'src/app/services/clip.service';
 import IClip from '../../models/clip.model';
 // import { ModalService } from 'src/app/services/modal.service';
 import { BehaviorSubject } from 'rxjs';
+import { ClipService } from '../../services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -19,7 +19,7 @@ export class ManageComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    // private clipService: ClipService,
+    private clipService: ClipService,
     // private modal: ModalService
   ) { 
     this.sort$ = new BehaviorSubject(this.videoOrder)
@@ -30,6 +30,7 @@ export class ManageComponent implements OnInit {
       this.videoOrder = params.sort === '2' ? params.sort : '1'
       this.sort$.next(this.videoOrder)
     })
+    this.clipService.getUserClips().subscribe(console.log);
   }
 
   sort(event: Event) {
