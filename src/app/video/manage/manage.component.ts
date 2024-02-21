ecf7dde2d40a77d0dc0ac5f19c210b559815d4d9
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-21 15:11:31 -0500

    kk2: cleanup manage component code

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -30,16 +30,6 @@ export class ManageComponent implements OnInit {
       this.videoOrder = params.sort === '2' ? params.sort : '1'
       this.sort$.next(this.videoOrder)
     })
-    // this.clipService.getUserClips(this.sort$).subscribe(docs => {
-    //   this.clips = []
-
-    //   docs.forEach(doc => {
-    //     this.clips.push({
-    //       docID: doc.id,
-    //       ...doc.data()
-    //     })
-    //   })
-    // })
   }
 
   sort(event: Event) {
@@ -52,46 +42,5 @@ export class ManageComponent implements OnInit {
     })
   }
 
-  openModal($event: Event, clip: IClip) {
-    // $event.preventDefault()
-
-    // this.activeClip = clip
-
-    // this.modal.toggleModal('editClip')
-  }
-
-  update($event: IClip) {
-    // this.clips.forEach((element, index) => {
-    //   if(element.docID == $event.docID) {
-    //     this.clips[index].title = $event.title
-    //   }
-    // })
-  }
-
-  deleteClip($event: Event, clip: IClip) {
-    // $event.preventDefault()
-
-    // this.clipService.deleteClip(clip)
-
-    // this.clips.forEach((element, index) => {
-    //   if(element.docID == clip.docID) {
-    //     this.clips.splice(index, 1)
-    //   }
-    // })
-  }
-
-  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
-    // $event.preventDefault()
-
-    // if(!docID) {
-    //   return
-    // }
-
-    // const url = `${location.origin}/clip/${docID}`
-
-    // await navigator.clipboard.writeText(url)
-
-    // alert('Link Copied!')
-  }
-
+  
 }
