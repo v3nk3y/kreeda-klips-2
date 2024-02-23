3818011ef99d1ce23c8d7aa15b145509ae2a8033
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-23 10:16:56 -0500

    kk2: determine the bottom of page logic for scrolling

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
   styleUrl: './clips-list.component.scss'
 })
-export class ClipsListComponent {
+export class ClipsListComponent implements OnInit, OnDestroy{
+  
+  constructor() {}
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { scrollTop,  offsetHeight} = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfPage = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if(bottomOfPage) {
+      console.log('Bottom of Page');
+    }
+  }
 }
