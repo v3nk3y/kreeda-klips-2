93e13d783e308b42b11fc9b5ca42a3d2d8da6450
v3nk3y <nagireddyvenkatesh@gmail.com>
2024-02-23 10:02:17 -0500

    kk2: add clips list component to seperate list of clips

---
+++ src/app/clips-list/clips-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clips-list',
+  templateUrl: './clips-list.component.html',
+  styleUrl: './clips-list.component.scss'
+})
+export class ClipsListComponent {
+
+}
